fd9cffccebc6fcbe02ddab664f5b2e35f3188851
Moreno Pradita <morenopradita013@gmail.com>
2025-09-08 11:37:46 +0700

    Commit No.4 – use jetpack navigation

--- app/src/main/java/com/example/lab_week_03/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_03/ListFragment.kt
@@ -1,6 +1,5 @@
 package com.example.lab_week_03
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -19,22 +18,10 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListFragment : Fragment(), View.OnClickListener{
+class ListFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    private lateinit var coffeeListener: CoffeeListener;
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if(context is CoffeeListener){
-            coffeeListener = context
-        }
-        else{
-            throw RuntimeException("Must implement CoffeeListener")
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,14 +48,13 @@ class ListFragment : Fragment(), View.OnClickListener{
 
         )
 
-        coffeeList.forEach{
-            it.setOnClickListener(this)
-        }
-    }
-
-    override fun onClick(v: View?){
-        v?.let{
-                coffee -> coffeeListener.onSelected(coffee.id)
+        coffeeList.forEach{ coffee ->
+            val fragmentBundle = Bundle()
+            fragmentBundle.putInt(COFFEE_ID, coffee.id)
+            coffee.setOnClickListener(
+                Navigation.createNavigateOnClickListener(
+                    R.id.coffee_id_action, fragmentBundle)
+            )
         }
     }
 
